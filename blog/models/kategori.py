99d5af4720f9f43c6c955c6c0c8919384195b563
hmzugur <craxx67@gmail.com>
2021-03-12 16:03:33 +0300

    Kategori model& verbose_name created

--- blog/models/kategori.py
+++ blog/models/kategori.py
@@ -3,8 +3,16 @@ from autoslug import AutoSlugField
 
 class KategoriModel(models.Model):
     isim = models.CharField(max_length=30, null= True, blank= True)
-    slug = AutoSlugField(populate_from='isim',unique=True) #Yukardaki isimden ->otomatik slug oluştur diyoruz.
+    slug = AutoSlugField(populate_from='isim',unique=True)
+    #Yukardaki isimden ->otomatik slug oluştur diyoruz.
 
 
     class Meta:
         db_table ='kategori'
+        verbose_name_plural = 'Kategoriler'
+        #Admin panelinde sol tarafta yazan Kategori modelsi düzelttik ve onun yerine
+        #Kategoriler yazdık
+        verbose_name = 'MyKategori'
+
+    def __str__(self):
+        return self.isim
