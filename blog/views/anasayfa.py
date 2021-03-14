59b9a33541d8e7f8f68f6ab61a2d730fe3ab0254
hmzugur <craxx67@gmail.com>
2021-03-14 16:39:49 +0300

    pagination,iconscdn, html&bs4

--- blog/views/anasayfa.py
+++ blog/views/anasayfa.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from blog.models import YazilarModel
-
+from django.core.paginator import Paginator #-> Paginator için
 
 def anasayfa(request):
-    yazilar = YazilarModel.objects.all()
+    yazilar = YazilarModel.objects.order_by("-id") #Enson  atılan yazıyı, en başa al.
+    sayfa=request.GET.get('sayfa')
+    paginator = Paginator(yazilar,2)
 
 
     context = {
-        'yazilar':yazilar
+        'yazilar':paginator.get_page(sayfa)
     }
     return render(request,"pages/anasayfa.html", context=context)
