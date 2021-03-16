09b6ed2144a4cf84bd8dbfbec87aca71c7129525
hmzugur <craxx67@gmail.com>
2021-03-17 02:15:18 +0300

    yorum_ekle.py eklendi

---
+++ blog/forms/yorum_ekle.py
@@ -0,0 +1,10 @@
+from django import forms
+from blog.models import YorumModel
+
+
+class YorumEkleModelForm(forms.ModelForm):
+    class Meta:
+        model = YorumModel
+        fields = ('yorum',)
+
+
