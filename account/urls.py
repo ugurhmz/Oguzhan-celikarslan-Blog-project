91adb18282b83a11a1330842a0fa3e354d0bbe42
hmzugur <craxx67@gmail.com>
2021-03-17 14:14:34 +0300

    cikis.py eklendi

---
+++ account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from account.views import cikis
+
+
+
+
+urlpatterns= [
+    path('cikis', cikis, name='cikis')
+]
--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,7 @@ from django.conf.urls.static import static         # -> static dosyalarımız i�
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
     path('', include('blog.urls')),
 ]
 
