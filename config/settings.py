0493a5ecf20659a1cd24f87c30aa8cf5c0d234a0
hmzugur <craxx67@gmail.com>
2021-03-17 20:05:27 +0300

    giris, kayıt

--- config/settings.py
+++ config/settings.py
@@ -99,9 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Istanbul'
 
 USE_I18N = True
 
@@ -137,6 +137,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL ='/'
 
 
 
