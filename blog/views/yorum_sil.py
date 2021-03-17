423c0abe3a8a664301530c9f32a5883c3e679e26
hmzugur <craxx67@gmail.com>
2021-03-17 03:01:20 +0300

    messages framework eklendi

--- blog/views/yorum_sil.py
+++ blog/views/yorum_sil.py
@@ -1,12 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from blog.models import YorumModel
 from django.shortcuts import  get_object_or_404, redirect
+from django.contrib import messages
+
+
 
 @login_required(login_url='/')
 def yorum_sil(request,id): #belli başlı sluglı olanı silecez
     yorum=get_object_or_404(YorumModel, id=id)
     if yorum.yazan == request.user or yorum.yazi.yazar ==request.user:
         yorum.delete()
+
+        messages.success(request,"Yorum Başarıyla silindi")
         return redirect('detay',slug=yorum.yazi.slug)
 
 
